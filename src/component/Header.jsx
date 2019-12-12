357852c068d58c03571e5da86c876c172d9b6b53
ch4o5 <1150477170@qq.com>
2019-12-12 10:32:58 +0800

    更新了头部样式
    Signed-off-by: ch4o5 <1150477170@qq.com>

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,27 +1,23 @@
-import React from "react";
-import {Image, Nav, Navbar} from 'react-bootstrap'
+import React from 'react';
+import {Col, Nav, Navbar} from 'react-bootstrap';
 
 // 头部组件
 
 export default () => (
     <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
-        <Navbar.Brand href="#home">
-            <Image
-                alt=""
-                src="/logo192.png"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-            />
-            九州第一群主页
-        </Navbar.Brand>
-        <Navbar.Collapse>
+      <Col lg={1}/>
+      <Navbar.Brand href="#home">
+        九州第一群主页
+      </Navbar.Brand>
+      <Navbar.Collapse>
             <Nav>
                 <Nav.Item>
-                    <Nav.Link href="https://9zer.doylee.cn/">九州第一群论坛</Nav.Link>
+                  <Nav.Link href="https://9zer.doylee.cn/"
+                            target="_blank">九州第一群论坛</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="https://novoland.doylee.cn/">九州页游（开发中）</Nav.Link>
+                  <Nav.Link href="https://novoland.doylee.cn/"
+                            target="_blank">九州页游（开发中）</Nav.Link>
                 </Nav.Item>
             </Nav>
         </Navbar.Collapse>
